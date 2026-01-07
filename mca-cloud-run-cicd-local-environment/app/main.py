40e2c52b0d2cefa6346e7641df8f7ddc31804e2d
mandeep <mandeepmails@gmail.com>
2026-01-07 17:15:31 +0000

    initial commit to the project repository for my mca project depicting cicd pipeline

---
+++ mca-cloud-run-cicd-local-environment/app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        app_name="Cloud CI/CD App hosted in the cloud service provider platform",
+        version=os.getenv("APP_VERSION", "1.0"),
+        deployed_at=datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"),
+        environment="Google Cloud Run"
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
